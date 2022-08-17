8f2762d6f77740251138679816b3856b80c4e3c4
emmanuelly15 <emmanuellynoma@gmail.com>
2022-08-17 16:55:17 +0200

    Work of the day: Before break.

---
+++ CommonModels/Model/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommonModels.Model
+{
+    public class LoginModel
+    {
+
+        public string email { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string password { get; set; }
+        [Required(ErrorMessage = "password is required")]
+
+    } 
+}
