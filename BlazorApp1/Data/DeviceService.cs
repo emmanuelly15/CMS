5502857a9d1ae9000ac6249ab8032bb663103936
allysonpietersen <allysonp24.ap@gmail.com>
2022-08-29 22:20:15 +0200

    device controller and services added

---
+++ BlazorApp1/Data/DeviceService.cs
@@ -0,0 +1,33 @@
+﻿using BlazorApp1.Pages;
+using CommonModels.Model;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class DeviceService
+    {
+        public async Task<int> SaveAsync(DeviceManagement device) //saving a device to the database
+        {
+            var client = new HttpClient();
+            var response = await client.PostAsync("https://localhost:44304/device", new StringContent(JsonConvert.SerializeObject(device), Encoding.UTF8, "application/json"));
+            var data = await response.Content.ReadAsStringAsync();
+
+
+            return int.Parse(data);
+        }
+
+        public async Task<DeviceManagement[]> GetDevicesAsync() //getting data from the database
+        {
+
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://localhost:44304/device");
+            var data = await response.Content.ReadAsStringAsync();
+
+            var listOfDevices = Newtonsoft.Json.JsonConvert.DeserializeObject<DeviceManagement[]>(data);
+            return listOfDevices;
+        }
+    }
+}
