0f3656b335b7aff89bf232424d86a2df5d4bfaef
allysonpietersen <allysonp24.ap@gmail.com>
2022-08-31 23:48:52 +0200

    document submission page connection to api

--- CommonModels/Model/Document.cs
+++ CommonModels/Model/Document.cs
@@ -3,11 +3,19 @@
     using System;
     public class Document
     {
-        public string Name { get; set; }
-        public DateTime? CreationDate { get; set; }
-        public string Creator { get; set; }
-        public string Url { get; set; }
-        public string CellphoneNumber { get; set; }
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int GroupId { get; set; }
+        public int DeviceId { get; set; }
+        public DateTime? SentDateTime { get; set; }
+        public string FileFormat { get; set; }
+        public string Img { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public bool Status { get; set; }
+        public string DocType { get; set; }
+        public int Amount { get; set; }
+        public string Comment { get; set; }
 
     }
 }
