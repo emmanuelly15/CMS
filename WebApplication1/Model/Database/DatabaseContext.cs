99923855ea60d91516c804b9e0497408894d0c36
allysonpietersen <allysonp24.ap@gmail.com>
2022-09-02 21:17:40 +0200

    document submission image connection

--- WebApplication1/Model/Database/DatabaseContext.cs
+++ WebApplication1/Model/Database/DatabaseContext.cs
@@ -14,13 +14,7 @@ namespace Api.Model.Database
         public DbSet<DbAdminUser> AdminUsers { get; set; }
         public DbSet<DbDevice> Devices { get; set; }
         public DbSet<DbGroup> Groups { get; set; }
-<<<<<<< HEAD
         public DbSet<DbMailingList> ML { get; set; }
-=======
-<<<<<<< HEAD
-        public DbSet<DbMailingList> ML { get; set; }
-=======
->>>>>>> Alpha-branch
         public DbSet<DbDocument> Documents { get; set; }
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
