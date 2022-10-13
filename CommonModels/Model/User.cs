7c1a638631d3afb07b40cf954eee151640ec945c
allysonpietersen <allysonp24.ap@gmail.com>
2022-10-13 16:42:54 +0200

    user password requirements added, done with shannon

--- CommonModels/Model/User.cs
+++ CommonModels/Model/User.cs
@@ -25,13 +25,14 @@ namespace CommonModels.Model
         public string Telephone { get; set; }
 
         //[RegularExpression(@"^.{8,}$")] //validate minimum amount of characters entered 
-        [RegularExpression(@"[^a-zA-Z''-'\s]+$", ErrorMessage = "Characters are not allowed.")]
+        [RegularExpression(@"[^a-zA-Z''-'\s]+$", ErrorMessage = "Characters are not allowed.")] //validate only digits enters for emp id
         [Required(ErrorMessage = "EmpId is required")]
        // [RegularExpression(@"^.{8,}$")] //validate minimum amount of characters entered 
-        [StringLength(8, MinimumLength = 8, ErrorMessage = "Employee Id must contain 8 characters.")] //validate maximum amount of characters entered and error message
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Employee Id must contain 8 characters.")] //validate max and minimum amount of characters entered and error message
         public string EmpId { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,32}$", ErrorMessage = "Password doesn't meet security rules.")]
         public string Password { get; set; }
 
         
