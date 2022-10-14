2e17010e6a94cedc0938968a68c24830a08c5404
allysonpietersen <allysonp24.ap@gmail.com>
2022-10-14 15:17:59 +0200

    device mgt validation added, edit device needs fixing

--- CommonModels/Model/Device.cs
+++ CommonModels/Model/Device.cs
@@ -11,8 +11,10 @@ namespace CommonModels.Model
     {
 
         public int Id { get; set; }
-
+       
+        [RegularExpression(@"[^a-zA-Z''-'\s]+$", ErrorMessage = "Characters are not allowed.")] //validate only digits enters for emp id
         [Required(ErrorMessage = "IMEI is required")]
+        [StringLength(15, MinimumLength = 15, ErrorMessage = "Employee Id must contain 15 digits.")] //validate max and minimum amount of characters entered and error message
         public string IMEI { get; set; }
 
         [Required(ErrorMessage = "User is required")]
